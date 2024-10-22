33a03f324457e29cd1ed96ab9f2db92b300d5256
Mariam-Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2024-10-22 23:01:15 +0400

    updates in environment

--- config/sequelize.js
+++ config/sequelize.js
@@ -2,13 +2,12 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS.replace(/'/g, ""),
+  // process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  // host: process.env.DB_HOST,
+  // port: 6543,
+  process.env.DATABASE_URL,
   {
-    host: process.env.DB_HOST,
     dialect: "postgres",
-    port: 6543,
   }
 );
 
